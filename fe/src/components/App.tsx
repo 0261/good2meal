44607859f33b6db71e7aa5d1a8342bbb20d36e4f
0261 <bada013@naver.com>
2019-09-27 23:43:56 +0900

    build: router

--- fe/src/components/App.tsx
+++ fe/src/components/App.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router';
+
+import Home from './Home/Home';
 
 function App() {
-    return <div>App</div>;
+    return (
+        <Switch>
+            <Route exact path='/' component={Home} />
+        </Switch>
+    );
 }
 
 export default App;
