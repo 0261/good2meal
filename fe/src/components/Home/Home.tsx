de5fd2eaafc5aebd7f3dcc9be3bad29eb31cf3b8
0261 <bada013@naver.com>
2019-09-29 12:26:24 +0900

    featrue: add test skeleton

--- fe/src/components/Home/Home.tsx
+++ fe/src/components/Home/Home.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Row, Col, Card, Affix, Button } from 'antd';
+import React, { useState } from 'react';
+import { Row, Col, Card, Affix, Button, Skeleton } from 'antd';
 
 import SearchBox from '../SearchBox/SearchBox';
 import TagBox from '../TagBox/TagBox';
@@ -7,23 +7,32 @@ import Meta from 'antd/lib/card/Meta';
 
 import styles from './Home.scss';
 
-const testCards = new Array(20).fill(true).map((d, i) => (
-    <Card
-        className={styles.Card}
-        key={i}
-        hoverable
-        cover={
-            <img
-                alt='example'
-                src='https://via.placeholder.com/300.png/09f/fff'
-            />
-        }
-    >
-        <Meta title='테스트' description='테스트 카드' />
-    </Card>
-));
-
 const Home = () => {
+    const [loading, setLoading] = useState(true);
+
+    new Promise(resolve => setTimeout(resolve, 1500)).then(() =>
+        setLoading(false),
+    );
+
+    const testCards = new Array(20).fill(true).map((d, i) => (
+        <Card
+            className={styles.Card}
+            key={i}
+            hoverable
+            cover={
+                <Skeleton loading={loading} avatar={{ size: 'large' }} active>
+                    <img
+                        alt='example'
+                        src='https://via.placeholder.com/300.png/09f/fff'
+                    />
+                </Skeleton>
+            }
+        >
+            <Skeleton loading={loading} active>
+                <Meta title='테스트' description='테스트 카드' />
+            </Skeleton>
+        </Card>
+    ));
     return (
         <div>
             <SearchBox></SearchBox>
