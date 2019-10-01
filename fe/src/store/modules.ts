cedc4ade5e71b34462d900bf9fad17615150f199
0261 <bada013@naver.com>
2019-10-01 22:46:14 +0900

    feature: add restaurant reducer

--- fe/src/store/modules.ts
+++ fe/src/store/modules.ts
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux';
+import restaurant from './restaurants/search';
 
-export const rootReducer = combineReducers({});
+export const rootReducer = combineReducers({
+    restaurant,
+});
 
 export type AppState = ReturnType<typeof rootReducer>;
---
+++ fe/src/store/restaurants/search.ts
@@ -0,0 +1,117 @@
+import produce from 'immer';
+// 액션
+const RESTAURANT_SEARCH = 'restaurant/search';
+
+interface RestaurantSearchAction {
+    type: typeof RESTAURANT_SEARCH;
+    payload: {
+        lastKey?: string | number;
+        filter: string;
+    };
+}
+type Action = RestaurantSearchAction;
+// 액션 생성자
+interface Search {
+    lastKey?: string | number;
+    filter?: string;
+}
+export const retaurantSearch = (searchParams: Search) => ({
+    type: RESTAURANT_SEARCH,
+    payload: {
+        ...searchParams,
+    },
+});
+
+// 리듀서
+const initState = {
+    restaurants: [
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+        {
+            imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+            alt: 'example',
+            title: '테스트',
+            desc: '테스트 카드',
+        },
+    ],
+};
+const restaurantReducer = (state = initState, action: Action) => {
+    return produce(state, draft => {
+        switch (action.type) {
+            case RESTAURANT_SEARCH:
+                draft.restaurants = draft.restaurants.concat({
+                    imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
+                    title: '12312444',
+                    alt: '123123',
+                    desc: '123123',
+                });
+                break;
+        }
+    });
+};
+
+export default restaurantReducer;
