cf2bf0220a6a2a8f87d4b8ac9a6fcfe1ed652e11
0261 <bada013@naver.com>
2019-10-01 22:49:00 +0900

    feature: add reducer

--- fe/src/containers/Home/Home.tsx
+++ fe/src/containers/Home/Home.tsx
@@ -1,87 +1,96 @@
 import React, { Component } from 'react';
+import { Dispatch } from 'redux';
+import { connect } from 'react-redux';
 
 import HomeComponent from '../../components/Home/Home';
+import { AppState } from '../../store/modules';
+import { retaurantSearch } from '../../store/restaurants/search';
 
-const restaurants = [
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-    {
-        imgUrl: 'https://via.placeholder.com/150.png/09f/fff',
-        alt: 'example',
-        title: '테스트',
-        desc: '테스트 카드',
-    },
-];
+interface Search {
+    lastKey?: string | number;
+    filter?: string;
+}
+interface Restaurant {
+    imgUrl: string;
+    alt: string;
+    title: string;
+    desc: string;
+}
+interface State {
+    loading: boolean;
+}
+interface Props {
+    restaurants: Array<Restaurant>;
+    restaurantSearch: (params: Search) => void;
+}
 
-interface State {}
-interface Props {}
+class Home extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            loading: true,
+        };
+        this.onInputSearch = this.onInputSearch.bind(this);
+        this.onLoading = this.onLoading.bind(this);
+        this.onTagSearch = this.onTagSearch.bind(this);
+    }
 
-export default class Home extends Component<State, Props> {
+    onLoading(loading: boolean) {
+        this.setState({ loading });
+        new Promise(resolve => setTimeout(resolve, 800)).then(() => {
+            this.props.restaurantSearch({});
+            this.setState({
+                loading: !loading,
+            });
+        });
+    }
+
+    onInputSearch(event: React.KeyboardEvent<HTMLInputElement>) {
+        const input = event.currentTarget.value;
+        this.props.restaurantSearch({
+            filter: input,
+        });
+    }
+
+    onTagSearch(tagName: string) {
+        this.props.restaurantSearch({
+            filter: tagName,
+        });
+    }
+
+    componentDidMount() {
+        new Promise(resolve => setTimeout(resolve, 800)).then(() =>
+            this.setState({
+                loading: !this.state.loading,
+            }),
+        );
+    }
+
     render() {
-        return <HomeComponent restaurants={restaurants} />;
+        return (
+            <HomeComponent
+                onLoading={this.onLoading}
+                restaurants={this.props.restaurants}
+                onSearch={this.onInputSearch}
+                onTagSearch={this.onTagSearch}
+                loading={this.state.loading}
+            />
+        );
     }
 }
+
+const mapStateToProps = (state: AppState) => {
+    return {
+        restaurants: state.restaurant.restaurants,
+    };
+};
+const mapDispatchToProps = (dispatch: Dispatch) => {
+    return {
+        restaurantSearch: (params: Search) => dispatch(retaurantSearch(params)),
+    };
+};
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps,
+)(Home);
