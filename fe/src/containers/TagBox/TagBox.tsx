4e8f828583401b9d05c34a772dfc482939243202
0261 <bada013@naver.com>
2019-10-05 01:18:04 +0900

    ffeature: add tags into componentDidMount

--- fe/src/containers/TagBox/TagBox.tsx
+++ fe/src/containers/TagBox/TagBox.tsx
@@ -2,27 +2,43 @@ import React, { Component } from 'react';
 
 import TagBoxComponent from '../../components/TagBox/TagBox';
 
-const tags = [
-    'magenta',
-    'red',
-    'volcano',
-    'orange',
-    'gold',
-    'lime',
-    'green',
-    'cyan',
-    'blue',
-    'geekblue',
-    'purple',
-];
-
-interface State {}
+interface State {
+    tags: Array<string>;
+}
 interface Props {
     onTagSearch: (tagName: string) => void;
 }
 
 export default class TagBox extends Component<Props, State> {
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            tags: [],
+        } as {
+            tags: Array<string>;
+        };
+    }
+
+    componentDidMount() {
+        const tags = [
+            'magenta',
+            'red',
+            'volcano',
+            'orange',
+            'gold',
+            'lime',
+            'green',
+            'cyan',
+            'blue',
+            'geekblue',
+            'purple',
+        ];
+        this.setState({
+            tags,
+        });
+    }
+
     render() {
-        return <TagBoxComponent onTagSearch={this.props.onTagSearch} tags={tags} />;
+        return <TagBoxComponent onTagSearch={this.props.onTagSearch} tags={this.state.tags} />;
     }
 }
