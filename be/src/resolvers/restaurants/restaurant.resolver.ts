d46b5fd1e637f7e3747a73925a183a8245400921
0261 <bada013@naver.com>
2019-10-07 23:52:25 +0900

    feature: restaurant dummy resolver

--- be/src/resolvers/restaurants/restaurant.resolver.ts
+++ be/src/resolvers/restaurants/restaurant.resolver.ts
@@ -1,13 +1,31 @@
 import { Resolver, Query, Arg, Args, Mutation } from 'type-graphql';
+import { Restaurant, QueryRestaurant } from './restaurant.model';
 
 @Resolver()
 export class RestaurantResolver {
-    @Query(returns => String)
-    async helloRestaurants() {
+    @Query(returns => [Restaurant])
+    queryRestaurants(): Array<Restaurant> {
         try {
-            return 'hello restaurants';
+            return [
+                {
+                    location: '구로디지털단지',
+                    id: '1',
+                },
+            ];
         } catch (error) {
             throw new Error(error);
         }
+    }
+
+    @Query(returns => String)
+    argRestaurants(@Arg('arg1', { description: 'arg' }) arg1: string): string {
+        console.log(arg1);
+        return 'arg';
+    }
+
+    @Query(returns => String)
+    argsRestauants(@Args() data: QueryRestaurant) {
+        console.log(data);
+        return 'args';
     }
 }
