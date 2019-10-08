3760afa1e9a87466ade9359d4acc51275746d156
0261 <bada013@naver.com>
2019-10-08 21:43:40 +0900

    feature. restaurant model

--- be/src/resolvers/restaurants/restaurant.model.ts
+++ be/src/resolvers/restaurants/restaurant.model.ts
@@ -1,4 +1,5 @@
 import { Field, ObjectType, ArgsType } from 'type-graphql';
+import { Category } from '../categories/category.model';
 
 @ObjectType({ description: '음식점 모델' })
 export class Restaurant {
@@ -6,14 +7,37 @@ export class Restaurant {
     location!: string;
     @Field({ description: '소트 키 ' })
     id!: string;
+
+    @Field({ nullable: true, description: '전화번호' })
+    tel?: string;
+    @Field({ nullable: true, description: '음식점 이름' })
+    name?: string;
+
+    @Field({ description: '보여질 음식점 이름' })
+    display!: string;
+    @Field({ description: '보여질 음식점 전화번호' })
+    telDisplay!: string;
+
+    @Field({ nullable: true, description: '음식점 주소' })
+    addr?: string;
+    @Field({ nullable: true, description: '음식점 영업시간' })
+    bizhourInfo?: string;
+    @Field(type => [String], {
+        nullable: true,
+        description: '음식점 대표 메뉴',
+    })
+    menuInfo?: Array<string>;
+    @Field(type => [String], { nullable: true, description: '음식점 해시태그' })
+    context?: Array<string>;
+
+    @Field(type => [Category], { description: '음식점 카테고리' })
+    category?: Array<Category>;
 }
 
 @ArgsType()
 export class QueryRestaurant {
+    @Field({ nullable: true })
+    imgUrl?: string;
     @Field()
     id!: string;
-    @Field({ nullable: true })
-    title?: string;
-    @Field({ nullable: true })
-    name?: string;
 }
