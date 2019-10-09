1fd44ee1fcd56463b2300ac6bf597a6e955fbe95
0261 <bada013@naver.com>
2019-10-09 17:47:13 +0900

    feature: restaurant resolver

--- be/src/resolvers/restaurants/restaurant.resolver.ts
+++ be/src/resolvers/restaurants/restaurant.resolver.ts
@@ -1,33 +1,19 @@
 import { Resolver, Query, Arg, Args, Mutation } from 'type-graphql';
 import { Restaurant, QueryRestaurant } from './restaurant.model';
+import { DynamoDB } from '../../services/dynamodb';
 
 @Resolver()
 export class RestaurantResolver {
-    @Query(returns => [Restaurant])
-    queryRestaurants(): Array<Restaurant> {
+    private readonly dynamodb: DynamoDB = new DynamoDB();
+    @Query(returns => [Restaurant], {
+        description: '음식점 검색',
+        name: 'GetRestaurants',
+    })
+    getRestaurants(): Array<Restaurant> {
         try {
-            return [
-                {
-                    location: '구로디지털단지',
-                    display: '1',
-                    id: '1',
-                    telDisplay: '1',
-                },
-            ];
+            return [];
         } catch (error) {
             throw new Error(error);
         }
-    }
-
-    @Query(returns => String)
-    argRestaurants(@Arg('arg1', { description: 'arg' }) arg1: string): string {
-        console.log(arg1);
-        return 'arg';
-    }
-
-    @Query(returns => String)
-    argsRestauants(@Args() data: QueryRestaurant) {
-        console.log(data);
-        return 'args';
     }
 }
