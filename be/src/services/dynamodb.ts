279e8f888bd96bf4192548fef7cf072900a1ae7d
0261 <bada013@naver.com>
2019-10-10 20:45:43 +0900

    feature: add put method

--- be/src/services/dynamodb.ts
+++ be/src/services/dynamodb.ts
@@ -1,5 +1,13 @@
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import * as expression from '@typemon/dynamodb-expression';
 
+interface Tables {
+    restaurant: {
+        location: string;
+        sortKey: string;
+        [key: string]: any;
+    };
+}
 export class DynamoDB {
     private static instance: DynamoDB;
     private client!: DocumentClient;
@@ -36,6 +44,19 @@ export class DynamoDB {
     public async get() {
         try {
             console.log('dynamodb');
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    public async put<K extends keyof Tables>(tableName: K, Item: Tables[K]) {
+        try {
+            const putOption = {
+                TableName: tableName,
+                Item,
+            };
+            console.log(putOption);
+            await this.client.put(putOption).promise();
         } catch (error) {
             throw error;
         }
