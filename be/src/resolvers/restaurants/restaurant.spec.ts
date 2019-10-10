e3addf7f3d424b0a8579b3e53c313f152907370d
0261 <bada013@naver.com>
2019-10-10 21:59:53 +0900

    feature: 음식점 생성 테스트코드

--- be/src/resolvers/restaurants/restaurant.spec.ts
+++ be/src/resolvers/restaurants/restaurant.spec.ts
@@ -1,5 +1,41 @@
-describe('restaurant test', () => {
-    it('restaurant gq', () => {
-        console.log('gq');
-    });
+import { graphqlCall } from '../../test-setup/graphqlCall';
+
+const dummy = [] as Array<any>;
+
+const createRestaurant = `
+mutation createRestaurants($data: CreateRestaurant!){
+    createRestaurants(
+        data: $data
+    )
+}
+`;
+
+describe('음식점', () => {
+    it('음식점 생성', async done => {
+        const jobs = dummy.map(async data => {
+            const restaurant = {
+                data: {
+                    location: '구로디지털단지',
+                    restaurantId: data.id,
+                    rank: data.rank,
+                    tel: data.tel,
+                    name: data.name,
+                    display: data.display,
+                    telDisplay: data.tel,
+                    category: data.category,
+                    context: data.context,
+                    menuInfo: data.menuInfo,
+                    bizhourInfo: data.bizhourInfo,
+                    roadAddress: data.roadAddress,
+                    address: data.address,
+                },
+            };
+            const result = await graphqlCall({
+                source: createRestaurant,
+                variableValues: restaurant,
+            });
+        });
+        await Promise.all(jobs);
+        await done();
+    }, 2500000);
 });
